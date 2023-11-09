b7117572c170d971753873e2c8eda36044fe3615
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-11-09 08:36:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/member/member.html',
+    controller: 'MemberController',
+    controllerAs: 'vm',
+    bindToController: true
+  };
+}git add member.js
